67a7d0f3e9c7e3916059a0e54db2e8c45b303700
MatteoOrlandini <48382335+MatteoOrlandini@users.noreply.github.com>
2020-10-30 19:00:26 +0100

    Update filter_design.m

--- Matlab/filter_design.m
+++ Matlab/filter_design.m
@@ -149,74 +149,8 @@ end
 
 
 %% real opamp design; Allen - CMOS analog circuit design pag. 293
-disp ("Allen - CMOS analog circuit design pag. 293")
-% MOSFET parameters
-k_n = 167 * 10^-6;  % u_n * Cox
-k_p = 42 * 10^-6;   % u_p * Cox
-V_t1min = 0.57;     % Minimum threshold voltage N MOS
-V_t1max = 0.93;     % Maximum threshold voltage N MOS
-V_t3max = -0.71;    % Threshold voltage P MOS
-% opamp parameters
-V_out_max = 3.3;    % max output voltage
-V_out_min = 0;      % min output voltage
-V_in_max = 3.1;     % max input voltage
-V_in_min = 1.2;     % min input voltage
-GB = 10*10^6;  % gain bandwidth (10 MHz = 2*pi*10*10^6 rad/s)
-SR = 6.22*10^6;       % slew rate (primo tentativo = 10V/us) 
-C_c = 5*10^-12;     % compensation capacitance primo tentativo = 5pF) 
-% calculation
-I_5 = SR * C_c;     % M5 drain current
-S_3 = I_5/(k_p*(V_out_max-V_in_max-abs(V_t3max)+V_t1min)^2); % M3 W/L 
-g_m1 = 2 * pi * GB * C_c;    % M1 g_m 
-I_1 = I_5/2;        % M1 drain current 
-S_1 = g_m1^2/(2*k_n*I_1); % M1 W/L
-V_ds5_sat = V_in_min - V_out_min - sqrt(I_5/(k_n*S_1)) - V_t1max;
-S_5 = 2*I_5/(k_n*V_ds5_sat^2); % M5 W/L
-g_m6 = 10 * g_m1;   % M6 g_m
-g_m4 = sqrt(2*k_p*S_3*I_1); %M4 g_m
-S_6 = S_3*g_m6/g_m4;% M6 W/L
-I_6 = g_m6^2/(2*k_p*S_6);   % M6 drain current
-S_7 = I_6/I_5*S_5;  % M7 W/L
-disp ("S_1 = S_2 = " + S_1);
-disp ("S_3 = S_4 = " + S_3);
-disp ("S_5 = S_8 = " + S_5);
-disp ("S_6 = " + S_6);
-disp ("S_7 = " + S_7);
-
-%% real opamp design; Allen - CMOS analog circuit design pag. 287
-disp ("Allen - CMOS analog circuit design pag. 287")
-% MOSFET parameters
-k_n = 167 * 10^-6;  % u_n * Cox
-k_p = 42 * 10^-6;   % u_p * Cox
-% opamp parameters
-A_0 = 1000;         % opamp DC gain
-GB = 10*10^6;  % gain bandwidth (10 MHz = 2*pi*10*10^6 rad/s)
-SR = 7*10^6;     % slew rate (primo tentativo = 10V/us), con SR=7.8V/us si migliora molto in DC offset
-C_c = 6*10^-12;     % compensation capacitance (primo tentativo = 5pF) 
-lambda = 0.175;
-I_5 = SR * C_c;     % M5 drain current
-g_m1 = 2 * pi * GB * C_c;    % M1 g_m
-A_v1 = 2*g_m1/(I_5*2*lambda); % first stage gain
-A_v2 = A_0 / A_v1;  % second stage gain
-g_m6 = 10*g_m1;     % M6 g_m
-I_6 = g_m6/(A_v2*2*lambda); % M6 drain current
-I_1 = I_5/2;        % M1 drain current
-S_1 = g_m1^2/(2*k_n*I_1); % W/L M1
-S_6 = g_m6^2/(2*k_p*I_6);   % W/L M6
-V_od5 = 0.3;        % overdrive voltage M5 = 0.2 V
-S_5 = 2*I_5/(k_n*V_od5^2);  % W/L M5
-S_7 = S_5*I_6/I_5;  % W/L M7
-V_od4 = V_od5;      % overdrive voltage M4 = 0.2 V
-S_3 = 2*I_1/(k_p*V_od4^2);  % W/L M3
-disp ("C_c = " + C_c);
-disp ("S_1 = S_2 = " + S_1);
-disp ("S_3 = S_4 = " + S_3);
-disp ("S_5 = S_8 = " + S_5);
-disp ("S_6 = " + S_6);
-disp ("S_7 = " + S_7);
-
-%% https://www.youtube.com/watch?v=pdIz5LkHOHM
-disp ("Design of two stage operational amplifier (opamp) - https://www.youtube.com/watch?v=pdIz5LkHOHM")
+% https://www.youtube.com/watch?v=pdIz5LkHOHM
+disp ("Design of two stage operational amplifier (opamp)")
 % MOSFET parameters
 k_n = 167 * 10^-6;  % u_n * Cox
 k_p = 42 * 10^-6;   % u_p * Cox
